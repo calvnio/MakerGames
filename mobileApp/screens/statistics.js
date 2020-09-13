895d545f0d24076cd2e952e9e0861886c92999a7
Calvin Tran <trancalvin31@gmail.com>
2020-09-14 01:02:59 +1000

    updated the PERFRORMANCE screen, still needs improvement

--- mobileApp/screens/statistics.js
+++ mobileApp/screens/statistics.js
@@ -1,22 +1,37 @@
-import React, { useState } from "react";
-import { Text, View, Dimensions, StyleSheet } from "react-native";
+import React from "react";
+import { Text, View, Dimensions, StyleSheet, Image } from "react-native";
+
+const barHeight = Dimensions.get("window").height / 1.75;
+const barWidth = Dimensions.get("window").width / 2;
 
 export default function Statistics({ navigation }) {
+  const performanceBar = StyleSheet.create({
+    container: {
+      width: barWidth,
+      height: barHeight,
+      borderRadius: 40,
+      backgroundColor: "#a9a9a9",
+      alignItems: "center",
+      justifyContent: "flex-end",
+    },
+    progress: {
+      width: barWidth,
+      height: barHeight / 2,
+      borderRadius: 40,
+      backgroundColor: "#7fff00",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text>This is the Statistics screen!</Text>
-      <View
-        style={{
-          width: Dimensions.get("window").width / 3,
-          height: Dimensions.get("window").height / 1.5,
-          borderRadius: 20,
-          backgroundColor: "light grey",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Text>Hello</Text>
+      <View style={performanceBar.container}>
+        <View style={performanceBar.progress}>
+          <Text>Sold $50k</Text>
+        </View>
       </View>
+      <PiggyBank />
     </View>
   );
 }
@@ -29,3 +44,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+const PiggyBank = (colour) => {
+  return (
+    <Image
+      source={require("../other_icons/pigbank.png")}
+      style={{ height: 100, width: 100, padding: 10 }}
+    />
+  );
+};
